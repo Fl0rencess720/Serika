99d6524910689732027a9d19e0a473c68e643955
Fl0rencess720 <2391793239@qq.com>
2025-02-07 20:22:59 +0800

    feat: support consul service registrar

--- example/server/server.go
+++ example/server/server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"crypto/tls"
+	"context"
 	"fmt"
 
+	"github.com/Fl0rencess720/Serika/registry"
 	"github.com/Fl0rencess720/Serika/server"
+	consulAPI "github.com/hashicorp/consul/api"
 )
 
 type Args struct {
@@ -15,29 +17,59 @@ type Reply struct {
 	C int
 }
 
-type Arith struct {
-}
+type Arith struct{}
 
 func (*Arith) Mul(args *Args, reply *Reply) error {
 	reply.C = args.A * args.B
 	return nil
 }
 
+func NewRegistrar(consulAddress string) (registry.ServiceRegister, error) {
+	config := consulAPI.DefaultConfig()
+	config.Address = consulAddress
+	consulClient, err := consulAPI.NewClient(config)
+	if err != nil {
+		return nil, err
+	}
+	return &registry.ConsulServiceRegister{ConsulClient: consulClient}, nil
+}
+
 func main() {
-	cert, err := tls.LoadX509KeyPair("../../server.pem", "../../server.key")
+	addr := "127.0.0.1:9002"
+	consulAddr := "127.0.0.1:8500"
+
+	registrar, err := NewRegistrar(consulAddr)
 	if err != nil {
 		fmt.Printf("err: %v\n", err)
 		return
 	}
-	server := server.NewServer(server.WithTLSConfig(
-		&tls.Config{Certificates: []tls.Certificate{cert}}),
-	)
+
+	// cert, err := tls.LoadX509KeyPair("../../server.pem", "../../server.key")
+	// if err != nil {
+	// 	fmt.Printf("err: %v\n", err)
+	// 	return
+	// }
+
+	// server := server.NewServer("example", server.WithTLSConfig(
+	// 	&tls.Config{Certificates: []tls.Certificate{cert}}),
+	// )
+	server := server.NewServer("example")
+	server.Metadata.Address = "127.0.0.1:9002"
+	// 注册服务
+	if err = registrar.Register(context.Background(), server); err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	// 绑定方法
 	err = server.Register("Arith", new(Arith))
 	if err != nil {
 		fmt.Printf("err: %v\n", err)
+		return
 	}
-	fmt.Println("start server on 127.0.0.1:9001......")
-	err = server.Serve("tcp", ":9001")
+
+	fmt.Println("start server on 127.0.0.1:9002......")
+
+	err = server.Serve("tcp", addr)
 	if err != nil {
 		fmt.Printf("err: %v\n", err)
 	}
